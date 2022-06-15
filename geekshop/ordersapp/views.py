21e71623eee6b0315faf483a8bd7cf0856c5c686
AlexPl2201 <alexpl2003@mail.ru>
2022-06-15 18:23:12 +0300

    Lesson 2_4

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,7 +1,9 @@
 from django.db import transaction
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
@@ -9,9 +11,9 @@ from django.views.generic import ListView, CreateView, UpdateView, DetailView, D
 
 from adminapp.mixin import BaseClassContextMixin
 from basket.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderItemsForm
 from ordersapp.models import Order, OrderItem
-
 
 class OrderList(ListView, BaseClassContextMixin):
     model = Order
@@ -39,7 +41,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
 
@@ -106,8 +108,35 @@ class OrderDelete(DeleteView, BaseClassContextMixin):
     success_url =  reverse_lazy('orders:list')
     title = 'GeekShop | Удаление заказа'
 
+
 def order_forming_complete(request, pk):
     order = Order.objects.get(pk=pk)
     order.status = Order.SEND_TO_PROCESSED
     order.save()
     return  HttpResponseRedirect(reverse('orders:list'))
+
+
+def get_product_price(request,pk):
+    if request.is_ajax():
+        product = Product.objects.get(pk=pk)
+        if product:
+            return JsonResponse({'price':product.price})
+        return JsonResponse({'price':0})
+
+
+@receiver(pre_save,sender=OrderItem)
+@receiver(pre_save,sender=Basket)
+def product_quantity_update_save(sender, instance, **kwargs):
+        if instance.pk:
+            item = instance.get_item(int(instance.pk))
+            instance.product.quantity -= instance.quantity - item
+        else:
+            instance.product.quantity -= instance.quantity
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+        instance.product.quantity += instance.quantity
+        instance.save()
